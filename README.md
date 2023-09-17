231ac35b65cc99f1c7b59fe33d153c8e732c68b8
David Johnson <david@bared.com.au>
2023-09-17 17:27:15 +1000

    Add infrastructure/app

---
+++ README.md
@@ -0,0 +1,86 @@
+# IOT Pool Temperature Sensor and Web App
+
+This project was inspired by my children, who kept asking to go outside to check if the pool was warm or not. Which prompted some questions:
+
+1) How warm is warm enough for everyone?
+2) How cold does the pool get in winter?
+3) Why are you still asking me to check the pool? Just look at the app!
+
+## AWS Infrastructure
+The sensor is backed by AWS, which collects temperature readings and serves the web app. The CDK project includes:
+
+- Route 53 hosted zones
+- Cloudfront distribution
+- S3 bucket
+- API Gateway w/ read & write Lambda integrations
+- RDS w/ MySQL
+
+To install dependencies:
+```bash
+cd infrastructure
+npm i
+```
+
+To deploy:
+```bash
+npm run deploy
+```
+
+To destroy:
+```bash
+npm run destroy
+```
+
+## Web App
+The web app is a simple Gatsby site which pulls the latest temperature reading.
+
+To install dependencies:
+
+```bash
+cd app
+npm i
+```
+
+To run:
+
+```bash
+npm run develop
+```
+
+To build:
+
+```bash
+npm run build
+```
+
+## Arduino project
+The code for the Arduino project is also included. You will need the following items for it to work:
+
+- [Arduino Uno R3 (or similar)](https://core-electronics.com.au/arduino-uno-r3.html)
+- [ProtoShield + Mini Breadboard for Arduino](https://core-electronics.com.au/protoshield-mini-breadboard-for-arduino.html
+)
+- [Temperature Sensor - Waterproof (DS18B20)](https://core-electronics.com.au/temperature-sensor-waterproof-ds18b20.html)
+- [Flanged Weatherproof Enclosure With PG-7 Cable Glands](https://core-electronics.com.au/flanged-weatherproof-enclosure-with-pg-7-cable-glands.html)
+- [5mm Pitch 2-Pin Screw Terminal Block](https://core-electronics.com.au/2-pin-screw-terminal-block-5mm-pitch.html)
+- Wires
+
+
+TODO:
+- Fritzing diagram
+- Add missing parts to README
+
+To run:
+```bash
+cd arduino/iot-pool-temperature-sensor
+cp credentials.h.example credentials.h
+```
+
+Update the credentials:
+```c++
+const char WIFI_SSID[] = "network";
+const char WIFI_PASSWORD[] = "password";
+const char API_GATEWAY_ENDPOINT[] = "api.{YOUR_DOMAIN}";
+const int API_GATEWAY_PORT = 443;
+```
+
+Connect the Arduino to your computer and then deploy via the Arduino IDE.
---
+++ arduino/iot-pool-temperature-sensor/iot-pool-temperature-sensor.ino
@@ -0,0 +1,122 @@
+#include "credentials.h"
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+#include <WiFiNINA.h>
+#include <ArduinoHttpClient.h>
+
+#define MIN_RSSI -80
+#define MAX_RSSI -50
+#define NUM_BARS 7
+#define INTERVAL 60000
+
+OneWire oneWire(2); 
+DallasTemperature sensors(&oneWire);
+WiFiSSLClient client;
+
+unsigned long lastMillis = 0;
+
+// This code reboots the Arduino if we're having trouble connecting to the Wi-Fi
+void(* resetFunc) (void) = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  sensors.begin();
+  connectWiFi();
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  if (millis() - lastMillis > INTERVAL || lastMillis == 0) {
+    lastMillis = millis();
+    publishMessage();      
+  }
+}
+
+void connectWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print(F("Connecting to "));
+  Serial.print(WIFI_SSID);
+  Serial.println("");
+
+  Serial.print("Signal strength: ");
+  Serial.println(rssiBars());
+
+  int connectionAttempts = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(6000);
+    Serial.print('.');
+    connectionAttempts++;
+
+    if (connectionAttempts > 10) {
+      Serial.println("Rebooting...");
+      Serial.println("");
+      resetFunc();
+    }
+  }
+
+  Serial.println('\n');
+  Serial.println(F("Connection established!"));
+  Serial.print(F("IP address:"));
+  Serial.println(WiFi.localIP());
+}
+
+int rssiBars() {
+  auto rssi = WiFi.RSSI();
+  if (rssi <= MIN_RSSI) {
+    return 0;
+  } else if (rssi >= MAX_RSSI) {
+    return NUM_BARS;
+  } else {
+    float inputRange = (MAX_RSSI - MIN_RSSI);
+    float outputRange = (NUM_BARS - 1);
+    return (int)( ((float)(rssi - MIN_RSSI) * outputRange / inputRange) + 0.5 );
+  }
+}
+
+void publishMessage() {
+  sensors.requestTemperatures();
+  
+  String postData = "{\"temperature\": " + String(sensors.getTempCByIndex(0)) + "}";
+
+  Serial.println(postData);
+
+  if (client.connectSSL(API_GATEWAY_ENDPOINT, API_GATEWAY_PORT)) {
+    client.print("POST ");
+    client.print(API_GATEWAY_PATH);
+    client.println(" HTTP/1.1");
+    client.print("POST ");
+    client.print(API_GATEWAY_PATH);
+    client.println(" HTTP/1.1");
+    client.print("Host: ");
+    client.println(API_GATEWAY_ENDPOINT);
+    client.println("User-Agent: Pool/1.0");
+    client.println("Content-Type: application/json");
+    client.print("x-api-key: ");
+    client.println(API_KEY);
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+    client.println();
+
+    while (client.connected()) {  
+      while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+      }
+    }
+
+    Serial.println("Closing connection.");
+    client.stop();
+  } else {
+    Serial.println("Could not send data");
+  }
+}
+
