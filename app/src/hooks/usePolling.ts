c0381aa5743d36b640975790aa32115d6f90f4b7
David Johnson <david@bared.com.au>
2023-09-27 11:26:23 +1000

    Apply formatting

--- app/src/hooks/usePolling.ts
+++ app/src/hooks/usePolling.ts
@@ -3,49 +3,49 @@ import { useState, useEffect, useRef, useCallback } from "react"
 const DEFAULT_POLLING_INTERVAL = 5000
 
 export function useInterval(callback: () => any, delay: number) {
-	const savedCallback = useRef<() => any | undefined>()
+  const savedCallback = useRef<() => any | undefined>()
 
-	useEffect(() => {
-		savedCallback.current = callback
-	}, [callback])
+  useEffect(() => {
+    savedCallback.current = callback
+  }, [callback])
 
-	useEffect(() => {
-		const tick = () => {
-			if (savedCallback.current) {
-				savedCallback.current()
-			}
-		}
-		if (delay !== null) {
-			const id = setInterval(tick, delay)
-			return () => clearInterval(id)
-		}
-	}, [delay])
+  useEffect(() => {
+    const tick = () => {
+      if (savedCallback.current) {
+        savedCallback.current()
+      }
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay)
+      return () => clearInterval(id)
+    }
+  }, [delay])
 }
 
 export function usePolling(
-	callback: (abortSignal: AbortSignal) => any,
-	interval = DEFAULT_POLLING_INTERVAL,
+  callback: (abortSignal: AbortSignal) => any,
+  interval = DEFAULT_POLLING_INTERVAL,
 ) {
-	const abortControllerRef = useRef(new AbortController())
-	const [lastUpdated, setLastUpdated] = useState<Date | null>(null)
+  const abortControllerRef = useRef(new AbortController())
+  const [lastUpdated, setLastUpdated] = useState<Date | null>(null)
 
-	useEffect(() => {
-		callback(abortControllerRef.current.signal)
-		setLastUpdated(new Date())
+  useEffect(() => {
+    callback(abortControllerRef.current.signal)
+    setLastUpdated(new Date())
 
-		return () => {
-			abortControllerRef.current.abort()
-		}
-	}, [])
+    return () => {
+      abortControllerRef.current.abort()
+    }
+  }, [])
 
-	const intervalFn = useCallback(() => {
-		if (document.hasFocus()) {
-			callback(abortControllerRef.current.signal)
-			setLastUpdated(new Date())
-		}
-	}, [])
+  const intervalFn = useCallback(() => {
+    if (document.hasFocus()) {
+      callback(abortControllerRef.current.signal)
+      setLastUpdated(new Date())
+    }
+  }, [])
 
-	useInterval(intervalFn, interval)
+  useInterval(intervalFn, interval)
 
-	return lastUpdated
+  return lastUpdated
 }
