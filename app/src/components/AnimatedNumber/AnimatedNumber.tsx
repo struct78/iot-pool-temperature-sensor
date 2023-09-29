81a77aa07b4ec31d5a97dd08cb17f341070ba92c
David Johnson <david@bared.com.au>
2023-09-29 14:38:36 +1000

    Update number component to transition to new numbers

--- app/src/components/AnimatedNumber/AnimatedNumber.tsx
+++ app/src/components/AnimatedNumber/AnimatedNumber.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react"
+import React, { FC, useCallback, useEffect } from "react"
 import { useState } from "react"
 
 type AnimatedNumberProps = {
@@ -10,7 +10,7 @@ type AnimatedNumberProps = {
 
 export const AnimatedNumber: FC<AnimatedNumberProps> = ({ onChange, duration = 100, startValue = 0.00, value = 0.00 }) => {
   const [currentValue, setCurrentValue] = useState(startValue)
-  const startTime = new Date().getTime()
+  const [startTime] = useState(new Date().getTime())
   let requestId: number = -1
 
   const easing = (x: number) => {
@@ -29,12 +29,13 @@ export const AnimatedNumber: FC<AnimatedNumberProps> = ({ onChange, duration = 1
     return (2 - Math.pow(2, -20 * x + 10)) / 2
   }
 
-  const animate = () => {
+  const animate = useCallback(() => {
     const now = new Date().getTime()
 
-    if (now - startTime <= duration) {
+    if (now - startTime < duration) {
       const percentage = (now - startTime) / duration
       const nextValue = value * easing(percentage)
+
       setCurrentValue(nextValue)
 
       if (onChange) {
@@ -43,13 +44,13 @@ export const AnimatedNumber: FC<AnimatedNumberProps> = ({ onChange, duration = 1
 
       requestId = requestAnimationFrame(animate)
     } else {
-      setCurrentValue(value)
+      setCurrentValue(Number(value))
     }
-  }
+  }, [startTime, onChange, setCurrentValue, value])
 
   useEffect(() => {
     requestId = requestAnimationFrame(animate)
-  }, [])
+  }, [value])
   
-  return <span>{currentValue === 0 ? "00.00" : currentValue.toFixed(2).padStart(5, "0")}</span>
+  return <span>{currentValue.toFixed(2).padStart(5, "0")}</span>
 }
